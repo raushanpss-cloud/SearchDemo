bf86157a5739b3309f578078cca28666cd24eadf
raushanpss-cloud <raushan.pss@gmail.com>
2025-08-27 01:34:08 +0530

    Code changes for exception middleware

--- Searchify/Program.cs
+++ Searchify/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddTransient<ErrorHandlingMiddleware>();
+//builder.Services.AddTransient<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddSingleton<IAuthenticateService, AuthenticateService>();
@@ -98,7 +98,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseMiddleware<ErrorHandlingMiddleware>();
+//app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
