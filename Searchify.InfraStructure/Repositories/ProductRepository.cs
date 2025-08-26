614d3e11a8dde1cde625237552c52479b26eb40e
raushanpss-cloud <raushan.pss@gmail.com>
2025-08-26 15:27:24 +0530

    Update ProductRepository.cs

--- Searchify.InfraStructure/Repositories/ProductRepository.cs
+++ Searchify.InfraStructure/Repositories/ProductRepository.cs
@@ -15,7 +15,7 @@ namespace Searchify.Infrastructure.Repositories
             _context = context;
 
             _products.Add(new Product { ProductId = 1, Name = "Mobile", Price = 299 , Stock =10, Description = "Latest smartphone with high performance and camera quality.", Category ="Electronics"});
-            _products.Add(new Product { ProductId = 1, Name = "Laptop", Price = 199, Stock = 15, Description = "Powerful laptop with 16GB RAM and 512GB SSD.", Category = "Electronics" });
+            _products.Add(new Product { ProductId = 2, Name = "Laptop", Price = 199, Stock = 15, Description = "Powerful laptop with 16GB RAM and 512GB SSD.", Category = "Electronics" });
         }
 
         public async Task<IEnumerable<Product>> GetAllProducts()
